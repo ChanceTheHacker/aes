b6acee2573ac3eae86c1c57864506408e6e45bd5
WCPeragine <ertyjunior@gmail.com>
2018-08-01 21:15:28 +0000

    cannot nest <a> in <a>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,8 @@ const style = {
 	width: '64px',
 }
 
+// needs fix at 525px width
+
 const Navbar = () => {
 	return (
 		<header className="bg-white black tc pv4 avenir">
@@ -15,14 +17,14 @@ const Navbar = () => {
 		  <h1 className="mt2 mb0 baskerville i fw1 f1">Alliance Environmental Systems</h1>
 		  <nav className="bt bb tc mw7 center mt4">
 		    <a className="f6 f5-l link bg-animate black hover-bg-lightest-blue dib pa3 ph4-l" href="/">Home</a>
-		    <a className="f6 f5-l link bg-animate black hover-bg-light-green dib pa3 ph4-l hide-child" href="/projects">Projects
-    		  <div class="child absolute">
+		    <div className="f6 f5-l link bg-animate black hover-bg-light-green dib pa3 ph4-l hide-child" href="/projects">Projects
+    		  <div className="child absolute">
 			    <a className="f6 f5-l link bg-animate black hover-bg-light-green dib pa3 br bb bl mt3 ph4-l" href="/project1">Project 1</a>
 			    <a className="f6 f5-l link bg-animate black hover-bg-light-green dib pa3 br bb ph4-l" href="/project2">Project 2</a>
 			    <a className="f6 f5-l link bg-animate black hover-bg-light-green dib pa3 br bb ph4-l" href="/project3">Project 3</a>
 			    <a className="f6 f5-l link bg-animate black hover-bg-light-green dib pa3 br bb ph4-l" href="/project4">Project 4</a>
 			  </div>
-		    </a>
+		    </div>
 		    <a className="f6 f5-l link bg-animate black hover-bg-light-blue dib pa3 ph4-l" href="/aboutus">About Us</a>
 		    <a className="f6 f5-l link bg-animate black hover-bg-light-purple dib pa3 ph4-l" href="/services">Services</a>
 		    <a className="f6 f5-l link bg-animate black hover-bg-light-pink dib pa3 ph4-l" href="/community">Community</a>
